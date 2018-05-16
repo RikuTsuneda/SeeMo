8a8a0f3143a7f464cddccd160653564f28a7c60d
RikuTsuneda <qoo.eater@gmail.com>
2018-05-16 14:36:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/inagakilab/see_mo/EmptyActivity.java
@@ -0,0 +1,13 @@
+package com.example.inagakilab.see_mo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EmptyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
